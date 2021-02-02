aa352c31f6caa67d5a984dc09971e75270c3103e
Steven Theron <steve.theron365@gmail.com>
2021-02-02 14:45:32 +0200

    Declaring a winner: 1. added method calculateWinner() to check if a player has won. If so, change the status to reflect the winner else the status will remain as the next player's turn.
    2. Changed Board's handleClick to return early by ignoring a click if someone has won the game OR if a Square is already filled.

--- src/index.js
+++ src/index.js
@@ -41,6 +41,9 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice(); //.slice() --> creates a copy of the squares array to modify instead of modifying the existing array.
+        if (calculateWinner(squares) || squares[i]) {   //  if there is a winner or the square is already filled then return.
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -65,7 +68,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -113,3 +122,24 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+///"Given an array of 9 squares, this function will check for a winner and return 'X', 'O', or null as appropriate."
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
